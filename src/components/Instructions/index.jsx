0b0529acd30d7eda32e6b47669897279049b416b
conmart <connor.a.martinelli@gmail.com>
2020-05-08 12:48:30 -0700

    fix styling issue and better close icon

--- src/components/Instructions/index.jsx
+++ src/components/Instructions/index.jsx
@@ -15,13 +15,13 @@ const Instructions = () => {
 
   return (
     <div className="instructionsContainer">
-      <span onClick={closeInstructions}>x</span>
-      <h4>Overview:</h4>
+      <span className="close" onClick={closeInstructions} />
+      <h4>Overview</h4>
       <p>
         Players take turns removing dots from the board. Whoever removes the
         last dot loses!
       </p>
-      <h4>Taking Your Turn:</h4>
+      <h4>Taking Your Turn</h4>
       <p>
         During your turn you must select at least one dot, and can then select
         as many additional dots as you would like provided they are all part of
@@ -35,7 +35,7 @@ const Instructions = () => {
         </a>{" "}
         for more info, good luck!
       </p>
-      <Button onClick={closeInstructions} text="Close" />
+      <Button onClick={closeInstructions} text="Back to Game" />
     </div>
   );
 };
